d003a4b9b4cce1b0c0243302375159906fa67527
TESSERACTPY <tesseractpykb186@gmail.com>
2020-08-01 15:45:20 +0530

    first

---
+++ app/src/main/java/com/example/whatsapp/AdminTabsAccessorAdapter.java
@@ -0,0 +1,59 @@
+package com.example.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class AdminTabsAccessorAdapter extends FragmentPagerAdapter
+{
+
+    public AdminTabsAccessorAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int i)
+    {
+        switch(i)
+        {
+            case 0:
+                BlockedMessagesFragment blockedMessagesFragment = new BlockedMessagesFragment();
+                return blockedMessagesFragment;
+
+            case 1:
+                ReportedMessagesFragment reportedMessagesFragment = new ReportedMessagesFragment();
+                return reportedMessagesFragment;
+
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+
+        switch(position)
+        {
+            case 0:
+                return "Blocked Messages";
+
+            case 1:
+                return "Reported Messages";
+
+            default:
+                return null;
+        }
+
+    }
+}
